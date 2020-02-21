3ede7740426e49e7a113af15e1d034d9a3ba76b4
MintyU <deathcat@outlook.kr>
2020-02-22 01:04:55 +0900

    weather included

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector('.js-weather');
+
+
+const API_KEY = '424d07b0e106e751eac6bac38228d24c';
 const COORD = "coords";
 
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
 function saveCoords(coordsObj) {
     localStorage.setItem(COORD, JSON.stringify(coordsObj));
 }
@@ -11,6 +25,7 @@ function handleGeoSuccess(position) {
         longitude : longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -26,7 +41,8 @@ function loadCoord() {
     if (loadedCoord === null) {
         askForCoord();
     } else {
-        // getWeather
+        const parsedCoord = JSON.parse(loadedCoord);
+        getWeather(parsedCoord.latitude, parsedCoord.longitude);
     }
 }
 
